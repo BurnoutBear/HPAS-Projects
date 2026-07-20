0d826f2ccc70f5b884d5fbe1c055576fe2bf59c4
Me-P-eM <marco01gll@gmail.com>
2026-07-20 19:51:25 +0200

    SE - fix email body

--- Social-Engineering/PhishingEmail/automail.py
+++ Social-Engineering/PhishingEmail/automail.py
@@ -10,48 +10,48 @@ import pytz
 TIMEZONE = "Europe/Rome"
 TZ = pytz.timezone(TIMEZONE)
 HTML_BODY = """\
-    <html>
-    <body>
-    <p>Gentile Contribuente,</p>
+<html>
+<body>
+<p>Gentile Contribuente,</p>
 
-    <p>
-    Le ricordiamo dell’abilitazione del domicilio digitale, entrato in vigore con il provvedimento del 7 ottobre 2024 
-    (<a href="https://www.agenziaentrate.gov.it/portale/documents/20143/6458356/ProvvComunicazioneDomiciliodigitaleSpeciale+del+7+ottobre+2024.pdf/f1ad2896-77fc-4a22-0e61-6acadce56a5d?t=1728376480652">provvedimento-pdf</a>),
-    che permette di comunicare un indirizzo PEC presso il quale eleggere il proprio domicilio digitale speciale al fine di ricevere atti e comunicazioni dell’Agenzia delle entrate e dell’Agenzia delle entrate-Riscossione.
-    </p>
+<p>
+Le ricordiamo dell’abilitazione del domicilio digitale, entrato in vigore con il provvedimento del 7 ottobre 2024 
+(<a href="https://www.agenziaentrate.gov.it/portale/documents/20143/6458356/ProvvComunicazioneDomiciliodigitaleSpeciale+del+7+ottobre+2024.pdf/f1ad2896-77fc-4a22-0e61-6acadce56a5d?t=1728376480652">provvedimento-pdf</a>),
+che permette di comunicare un indirizzo PEC presso il quale eleggere il proprio domicilio digitale speciale al fine di ricevere atti e comunicazioni dell’Agenzia delle entrate e dell’Agenzia delle entrate-Riscossione.
+</p>
 
-    <p>
-    Ai sensi dell'art. 3-bis, comma 1-bis, del d.lgs 7 marzo 2005, n. 82
-    (Codice dell'Amministrazione Digitale - CAD), qualunque persona fisica ha facoltà di eleggere e modificare il proprio domicilio digitale, da iscrivere nell'Indice nazionale dei domicili digitali (INAD).
-    </p>
+<p>
+Ai sensi dell'art. 3-bis, comma 1-bis, del d.lgs 7 marzo 2005, n. 82
+(Codice dell'Amministrazione Digitale - CAD), qualunque persona fisica ha facoltà di eleggere e modificare il proprio domicilio digitale, da iscrivere nell'Indice nazionale dei domicili digitali (INAD).
+</p>
 
-    <p>
-    Per professionisti iscritti in albi o elenchi (come definiti ai sensi degli art. 6-bis e 6-quinques del d.lgs n.82/2005 CAD) l'elezione del domicilio digitale è un obbligo e La esortiamo ad eleggere al piú presto il proprio domicilio digitale.
-    </p>
+<p>
+Per professionisti iscritti in albi o elenchi (come definiti ai sensi degli art. 6-bis e 6-quinques del d.lgs n.82/2005 CAD) l'elezione del domicilio digitale è un obbligo e La esortiamo ad eleggere al piú presto il proprio domicilio digitale.
+</p>
 
-    <p>
-    Le ricordiamo che la comunicazione per il domicilio digitale speciale può avvenire esclusivamente online, utilizzando lo specifico servizio web disponibile in area riservata.
-    </p>
+<p>
+Le ricordiamo che la comunicazione per il domicilio digitale speciale può avvenire esclusivamente online, utilizzando lo specifico servizio web disponibile in area riservata.
+</p>
 
-    <p>
-    La presente comunicazione ha finalità esclusivamente informative e non costituisce un avviso di accertamento né una richiesta di pagamento.
-    </p>
+<p>
+La presente comunicazione ha finalità esclusivamente informative e non costituisce un avviso di accertamento né una richiesta di pagamento.
+</p>
 
-    <p>
-    Ulteriori indicazioni sono disponibili sul sito dell’Agenzia delle Entrate al seguente avviso:
-    <a href="https://www.agenziaentrate.gov.it/portale/comunicazione-del-domicilio-digitale-speciale/che-cos-cittadini">
-    Comunicazione del domicilio digitale speciale
-    </a>
-    </p>
+<p>
+Ulteriori indicazioni sono disponibili sul sito dell’Agenzia delle Entrate al seguente avviso:
+<a href="https://www.agenziaentrate.gov.it/portale/comunicazione-del-domicilio-digitale-speciale/che-cos-cittadini">
+Comunicazione del domicilio digitale speciale
+</a>
+</p>
 
-    <p>
-    Cordiali saluti,<br>
-    Il Direttore della Direzione Centrale Servizi ai Contribuenti
-    </p>
+<p>
+Cordiali saluti,<br>
+Il Direttore della Direzione Centrale Servizi ai Contribuenti
+</p>
 
-    </body>
-    </html>
-    """
+</body>
+</html>
+"""
 OUTPUT_FILE = "postacert.eml"
 
 @dataclass
